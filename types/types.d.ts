3aec07d2fd9abe0faf57b1458c671028f93fb804
fernandobouchet <fernandobouchet@gmail.com>
2024-10-18 23:45:20 -0300

    feat: add 'types' file

---
+++ types/types.d.ts
@@ -0,0 +1,31 @@
+type Quote = {
+  id: number;
+  text: string;
+  song: string;
+  band: string;
+  author: Profile;
+  authorId: string;
+  likes: Like[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+type Profile = {
+  id: string;
+  name: string;
+  full_name: string;
+  email: string;
+  avatar_url: string;
+  quotes: Quote[];
+  likes: Like[];
+  createdAt: Date;
+};
+
+type Like = {
+  id: number;
+  user: Profile;
+  userId: string;
+  quote: Quote;
+  quoteId: number;
+  createdAt: Date;
+};
