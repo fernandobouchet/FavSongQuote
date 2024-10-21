3f5178da962c71e42d1fc77d8a3c4ab10e7f7082
fernandobouchet <fernandobouchet@gmail.com>
2024-10-20 23:54:56 -0300

    feat: update main page and 'quoteCardsContainer' styles

--- components/quoteCardsContainer.tsx
+++ components/quoteCardsContainer.tsx
@@ -5,8 +5,8 @@ export default async function QuoteCardsContainer() {
   const quotes = await getAllQuotes();
 
   return (
-    <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-4">
-      {quotes.map((quote: Quote) => (
+    <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      {quotes?.map((quote: Quote) => (
         <QuoteCard key={quote.id} quote={quote} />
       ))}
     </div>
