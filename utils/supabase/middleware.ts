109da4b298dc3227ffe8c503e7a4b2236a5a8c37
fernandobouchet <fernandobouchet@gmail.com>
2024-10-20 21:57:16 -0300

    feat: update supabase utils

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -33,11 +33,9 @@ export async function updateSession(request: NextRequest) {
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
 
-  /*
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  await supabase.auth.getUser();
 
+  /*
   if (!user && !request.nextUrl.pathname.startsWith("/login")) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
