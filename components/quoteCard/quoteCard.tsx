1e2edc3fb606c382eefd1d644942631cb2600935
fernandobouchet <fernandobouchet@gmail.com>
2024-10-21 20:50:06 -0300

    feat: 'quoteCard' remove user info

--- components/quoteCard/quoteCard.tsx
+++ components/quoteCard/quoteCard.tsx
@@ -1,16 +1,13 @@
 "use client";
-import { Music, User } from "lucide-react";
+import { Music } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
 import QuoteLike from "./quoteLike";
-import { getFormattedDate } from "@/utils/functions";
 
 interface Props {
   quote: Quote;
 }
 
 export default function QuoteCard({ quote }: Props) {
-  const date = getFormattedDate(quote.createdAt);
-
   return (
     <Card key={quote.id} className="h-full bg-card text-card-foreground">
       <CardContent className="relative p-6 text-center">
@@ -28,15 +25,6 @@ export default function QuoteCard({ quote }: Props) {
           <p className="text-sm text-gray-600 dark:text-gray-400">
             {quote.band}
           </p>
-        </div>
-        <div className="mt-4 pt-4 border-t border-gray-200 dark:border-gray-600 flex justify-between items-center">
-          <div className="flex items-center text-sm text-gray-500 dark:text-gray-400">
-            <User className="h-4 w-4 mr-1" />
-            {quote.author?.name.split(" ")[0]}
-          </div>
-          <span className="text-xs text-gray-400 dark:text-gray-500">
-            {date}
-          </span>
         </div>
       </CardContent>
     </Card>
