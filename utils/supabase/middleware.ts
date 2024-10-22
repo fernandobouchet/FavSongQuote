88259b0c4140de7c6696c7b79fb53ef139d366a0
fernandobouchet <fernandobouchet@gmail.com>
2024-10-22 19:45:26 -0300

    feat: update supabase 'middleware'

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -33,15 +33,16 @@ export async function updateSession(request: NextRequest) {
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
 
-  await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  /*
-  if (!user && !request.nextUrl.pathname.startsWith("/login")) {
+  if (user && request.nextUrl.pathname.match("/login")) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
-    url.pathname = "/login";
+    url.pathname = "/";
     return NextResponse.redirect(url);
-  }*/
+  }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
